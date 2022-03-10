04219763f1f9dae4c05e913a71843507a2d9de1c
sunguanyong <1067946977@qq.com>
2022-03-10 14:30:26 +0800

    isModele_Main = true   主项目   isModule_A = false     可单独运行
      isModuleA_To_Main=true 是否添加到主项目,需设置moduleA为library

--- AndroidProject/app/src/main/java/com/sun/module_main/MainActivity.java
+++ AndroidProject/app/src/main/java/com/sun/module_main/MainActivity.java
@@ -1,5 +1,7 @@
 package com.sun.module_main;
 
+import android.content.Intent;
+
 import com.sun.common_library.base.BaseActivity;
 
 public class MainActivity extends BaseActivity {
@@ -11,11 +13,10 @@ public class MainActivity extends BaseActivity {
 
     @Override
     protected void initView() {
-
+        startActivity(new Intent(this, com.sun.module_a.MainActivity.class));
     }
 
     @Override
     protected void initData() {
-
     }
 }
